401f12364dc33afdc956609b34fd95714851b530
pu4j4 <bhanubethala2@gmail.com>
2026-04-11 22:26:46 +0530

    updated urls.py

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('verify/<path:token>/', views.verify_user, name="verify"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path("campaign/create/", views.create_campaign, name="create-campaign"),
-    path('contacts/upload/', views.upload_contacts, name="upload_contacts"),
+    path('contacts/upload/', views.upload_contacts, name="upload-contacts"),
+    path('campaign/send/<int:campaign_id>/', views.send_campaign, name="send-campaign"),
+    path('messages/process/', views.process_messages, name="process-messages"),
 ]
